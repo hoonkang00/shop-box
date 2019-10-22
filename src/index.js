936a2ddcb8bcea1ddc301d703d248ad17f382931
cferdinand <chrisferdinand07@gmail.com>
2019-10-22 12:10:05 -0400

    updated index.js to connect to store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
 import "./styles.css";
 import "./styles.scss";
-
+import App from "./components/app.jsx";
+import store from "./store/store.js";
+import { Provider } from "react-redux";
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  mountNode
+);
