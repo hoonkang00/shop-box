9a7185f0f7e246307b34163d5a54c4927221e893
Evelyn Chen <evelync93@gmail.com>
2019-10-22 17:19:22 -0400

    add QA Set component

--- src/components/questionComponents/QuestionAnswer.jsx
+++ src/components/questionComponents/QuestionAnswer.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SearchQuestions from "./SearchQuestions.jsx";
+import QASet from "./QASet.jsx";
 
 export default function QuestionAnswer() {
   return (
     <div>
       QUESTIONS & ANSWERS
       <SearchQuestions />
-      QUESTION ANSWER QUESTION ANSWER more question button add question button
+      <QASet />
+      more question button add question button
     </div>
   );
 }
