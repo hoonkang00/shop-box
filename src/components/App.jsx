e1cc8c10e61673cc773e89be77771cd775917462
pschwagler <pschwagl@gmail.com>
2019-10-22 16:19:18 -0400

    changed app.jsx to be uppercase and updated product details component

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProductDetails from "./ProductDetails.jsx";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ProductDetails />
+      </div>
+    );
+  }
+}
+
+export default App;
