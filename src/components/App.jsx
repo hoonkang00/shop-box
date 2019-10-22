0b4f29983963318b9b4677992a5407c1cec01d5d
Evelyn Chen <evelync93@gmail.com>
2019-10-22 16:53:43 -0400

    add qa component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ProductDetails from "./ProductDetails.jsx";
+import QuestionAnswer from "./questionComponents/QuestionAnswer.jsx";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <ProductDetails />
+        <QuestionAnswer/>
       </div>
     );
   }
