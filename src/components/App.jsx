e0accba4d31be794ee5b00ab4199bba969bd6914
Hoon Kang <hoonkang00@gmail.com>
2019-10-22 17:09:48 -0400

    npm install redux-dev tool, created a reducer/action to get product id and store it to the store

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ProductDetails from "./ProductDetails.jsx";
+import RelatedItemsContainer from "../containers/RelatedContainer.js";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <ProductDetails />
+        <RelatedItemsContainer />
       </div>
     );
   }
