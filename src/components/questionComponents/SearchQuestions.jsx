db9b83bafd3f4a170e3c4f1f9b1cb0b8e8c530ad
Evelyn Chen <evelync93@gmail.com>
2019-10-23 12:04:10 -0400

    add search bar

--- src/components/questionComponents/SearchQuestions.jsx
+++ src/components/questionComponents/SearchQuestions.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
+import SearchBar from "material-ui-search-bar";
 
 export default class SearchQuestions extends Component {
   constructor(props) {
     super(props);
     this.state = { searchTerm: "" };
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange(e) {
+    console.log(e);
   }
   render() {
-    return <div>search bar</div>;
+    return (
+      <SearchBar
+        onChange={e => this.onChange(e)}
+        onRequestSearch={() => console.log("onRequestSearch")}
+        style={{
+          margin: "0 auto",
+          maxWidth: 800
+        }}
+        value={this.state.searchTerm}
+      />
+    );
   }
 }
