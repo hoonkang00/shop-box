0cb918fcd509b0ec4e99a25165bc0db371dc30c1
Hoon Kang <hoonkang00@gmail.com>
2019-10-24 12:00:58 -0400

    changed the cards from material-ui grid to cards. Used react-item-carousel to make the carousel. Added the function to update the store's current product

--- src/containers/RelatedItemsContainers/ItemListContainer.js
+++ src/containers/RelatedItemsContainers/ItemListContainer.js
@@ -1,6 +1,8 @@
 import { connect } from "react-redux";
 import store from "../../store/store.js";
 import ItemList from "../../components/RelatedItemsComponents/ItemList.jsx";
+import getProductInfo from "../../actions/getProductInfo.js";
+import getReviewMetaData from "../../actions/getReviewMetaData.js";
 
 const mapStateToProps = (store, oldProps) => {
   return {
@@ -9,18 +11,17 @@ const mapStateToProps = (store, oldProps) => {
   };
 };
 
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     setStoreProductInfo: id => {
-//       return dispatch(getProductInfo(id));
-//     },
+const mapDispatchToProps = dispatch => {
+  return {
+    setStoreProductInfo: id => {
+      return dispatch(getProductInfo(id));
+    }
+  };
+};
 
-//     setStoreReviewMetaData: id => {
-//       return dispatch(getReviewMetaData(id));
-//     }
-//   };
-// };
-
-const ItemListContainer = connect(mapStateToProps)(ItemList);
+const ItemListContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ItemList);
 
 export default ItemListContainer;
