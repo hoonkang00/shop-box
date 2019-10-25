935aa1930c7fd701d1835f4d5ff335bfd58d7fd9
cferdinand <chrisferdinand07@gmail.com>
2019-10-25 14:12:10 -0400

    added filter function to the stars

--- src/actions/getReviewList.js
+++ src/actions/getReviewList.js
@@ -1,18 +1,14 @@
 import axios from "axios";
 
-const getReviewList = ([page, actionType, options = {}]) => {
+const getReviewList = ([page, actionType, sort = ""]) => {
   const productID = parseInt(
     window.location.href.split("products/")[1].substring(0, 1)
   );
 
-  let queryOptions = {
-    rating: options.rating || "",
-    sort: options.sort || ""
-  };
   return dispatch => {
     return axios
       .get(
-        `http://18.223.1.30/reviews/${productID}/list?count=2&page=${page}&rating=${queryOptions.rating}&sort=${queryOptions.sort}`
+        `http://18.223.1.30/reviews/${productID}/list?count=2&page=${page}&sort=${sort}`
       )
       .then(({ data }) => {
         dispatch({
