9d589ec00787498d1b6ae72726c3cd26e9e6bf4a
cferdinand <chrisferdinand07@gmail.com>
2019-10-26 10:59:40 -0400

    characteristics added

--- src/components/ReviewsComponents/CharacteristicsEntry.jsx
+++ src/components/ReviewsComponents/CharacteristicsEntry.jsx
@@ -3,13 +3,29 @@ import Grid from "@material-ui/core/Grid";
 import Slider from "@material-ui/core/Slider";
 
 const CharacteristicsEntry = ({ characteristicName, characteristicValue }) => {
+  const marks = [
+    {
+      value: 0,
+      label: characteristicName === "Size" ? "Too small" : "Poor"
+    },
+    {
+      value: 50,
+      label: characteristicName === "Size" ? "Perfect" : "Good"
+    },
+    {
+      value: 100,
+      label: characteristicName === "Size" ? "Too big" : "Great"
+    }
+  ];
   return (
     <div>
       <Grid item xs className={`characteristic-${characteristicName}`}>
         <span className={"characteristic"}>{characteristicName}</span>
         <Slider
-          value={Number(characteristicValue)}
+          value={Number((characteristicValue.value / 5) * 100)}
           aria-labelledby="input-slider"
+          className="char"
+          marks={marks}
         />
       </Grid>
     </div>
