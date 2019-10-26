15661f14bd68f10a10eab5e97ea4d9e61975a668
pschwagler <pschwagl@gmail.com>
2019-10-26 16:19:52 -0400

    added image thumbnails overlaying the default image

--- src/components/Product Details/DefaultImageView.jsx
+++ src/components/Product Details/DefaultImageView.jsx
@@ -1,9 +1,30 @@
 import React, { useState } from "react";
 
 export default ({ photos }) => {
+  if (photos === undefined) {
+    return <></>;
+  }
   const [selectedIndex, updateSelectedIndex] = useState(0);
-
+  const [firstIndex, updateFirstIndex] = useState(0);
   let mainPhoto = photos ? photos[selectedIndex].url : "";
+  let displayedPhotos = [];
+  for (let i = 0; i < photos.length; i++) {
+    if (i >= firstIndex && i < firstIndex + 5) {
+      displayedPhotos.push([photos[i].url, i]);
+    }
+  }
 
-  return <img className="main-img" src={mainPhoto}></img>;
+  console.log(displayedPhotos);
+
+  return (
+    <div className="main-img" style={{ backgroundImage: `url(${mainPhoto})` }}>
+      {displayedPhotos.map(([imgUrl, index]) => (
+        <div
+          className="list-image"
+          style={{ backgroundImage: `url(${imgUrl})` }}
+          onClick={() => updateSelectedIndex(index)}
+        ></div>
+      ))}
+    </div>
+  );
 };
