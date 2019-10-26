72974183f1568fd7621981e469b8c5945592784c
Evelyn Chen <evelync93@gmail.com>
2019-10-26 12:41:49 -0400

    add reported button for answers

--- src/components/questionComponents/Answers.jsx
+++ src/components/questionComponents/Answers.jsx
@@ -32,8 +32,17 @@ export default function Answers({ answer, getAnswers }) {
         console.log(err);
       });
   };
-  const [reported, updateReported] = useState(false);
-
+  const [reported, markReported] = useState(false);
+  const reportReview = () => {
+    axios
+      .put(`http://18.223.1.30/qa/answer/${answer.answer_id}/report`)
+      .then(() => {
+        markReported(true);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   return (
     <div className="answer-block">
       <div>
@@ -55,14 +64,11 @@ export default function Answers({ answer, getAnswers }) {
         {"("}
         {answer.helpfulness}
         {")"} &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-        <span
-          className="yes-button"
-          onClick={() => {
-            console.log("yes working");
-          }}
-        >
-          Report
-        </span>
+        {reported ? (
+          <span style={{ color: "red" }}>Reported!</span>
+        ) : (
+          <span onClick={reportReview}>Report</span>
+        )}
       </div>
     </div>
   );
