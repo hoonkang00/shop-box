56cc0e0deec90ad51c3f5d812d627cb350184ff4
cferdinand <chrisferdinand07@gmail.com>
2019-10-25 22:57:34 -0400

    added put requests for helpful and report

--- src/actions/updateHelpfulness.js
+++ src/actions/updateHelpfulness.js
@@ -1,29 +1,11 @@
 import axios from "axios";
 
-const updateHelpfulness = ([reviewId]) => {
-  return dispatch => {
-    return axios
-      .put(`http://18.223.1.30/reviews/helpful/${reviewId}`)
-      .then(({ data }) => {
-        let filteredList = [];
-        data.results.forEach(review => {
-          if (Number(review.rating) === Number(rating)) {
-            filteredList.push(review);
-          }
-        });
-        data.results = filteredList;
-        return data;
-      })
-      .then(data => {
-        dispatch({
-          type: actionType === "REVIEWS" ? "REVIEWS" : "UPDATE-REVIEWS",
-          payload: data
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+const updateHelpfulness = reviewId => {
+  return axios
+    .put(`http://18.223.1.30/reviews/helpful/${reviewId}`)
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 export default updateHelpfulness;
--- src/components/ReviewsComponents/ReviewListEntry.jsx
+++ src/components/ReviewsComponents/ReviewListEntry.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Grid from "@material-ui/core/Grid";
 import Divider from "@material-ui/core/Divider";
 import Stars from "./StarRatings.jsx";
 import CheckIcon from "@material-ui/icons/Check";
+import updateFunction from "../../actions/updateHelpfulness.js";
+import report from "../../actions/reportReview.js";
 
 const ReviewListEntry = ({ review }) => {
   const renderResponse = !["null", null, 0].includes(review.response);
+  const [helpful, setHelpful] = useState(review.helpfulness);
+  const [reported, setReported] = useState(false);
+
+  const updateHelpfulness = () => {
+    updateFunction(review.review_id);
+    setHelpful(helpful + 1);
+  };
+
+  const reportReview = () => {
+    report(review.review_id);
+    setReported(true);
+  };
+
   return (
     <div className="rowEntry">
       <Grid container>
@@ -32,11 +47,15 @@ const ReviewListEntry = ({ review }) => {
         ) : (
           ""
         )}
-        <div>
+        <div className="helpful-report-container">
           <span>Helpful?</span>
-          {/* <span onClick={}>Yes ({review.helpfulness})</span> */}
+          <span onClick={updateHelpfulness}>Yes ({helpful})</span>
           <span>|</span>
-          <span>Report</span>
+          {reported ? (
+            <span style={{ color: "red" }}>Reported!</span>
+          ) : (
+            <span onClick={reportReview}>Report</span>
+          )}
         </div>
         <Divider />
       </Grid>
