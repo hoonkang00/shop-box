2a49612b95b12f171af4e368ec9ca5a23cd85927
pschwagler <pschwagl@gmail.com>
2019-10-26 17:16:06 -0400

    added up and down arrows on image list

--- src/components/Product Details/DefaultImageView.jsx
+++ src/components/Product Details/DefaultImageView.jsx
@@ -1,4 +1,20 @@
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    color: "rgba(0,0,0,.5)",
+    margin: "0 auto"
+  },
+  arrowTop: {
+    marginTop: "-5px"
+  },
+  arrowBottom: {
+    transform: "rotate(180deg)",
+    marginTop: "-22px"
+  }
+}));
 
 export default ({ photos }) => {
   if (photos === undefined) {
@@ -13,18 +29,24 @@ export default ({ photos }) => {
       displayedPhotos.push([photos[i].url, i]);
     }
   }
-
-  console.log(displayedPhotos);
+  const classes = useStyles();
 
   return (
     <div className="main-img" style={{ backgroundImage: `url(${mainPhoto})` }}>
+      <div className={classes.root}>
+        <ExpandLessIcon className={classes.arrowTop} />
+      </div>
       {displayedPhotos.map(([imgUrl, index]) => (
         <div
+          key={index}
           className="list-image"
           style={{ backgroundImage: `url(${imgUrl})` }}
           onClick={() => updateSelectedIndex(index)}
         ></div>
       ))}
+      <div className={classes.root}>
+        <ExpandLessIcon className={classes.arrowBottom} />
+      </div>
     </div>
   );
 };
