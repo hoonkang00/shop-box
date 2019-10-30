61a716f1b13f1e70a09bd8ff6fe8eb468d6eac22
pschwagler <pschwagl@gmail.com>
2019-10-30 11:42:07 -0400

    optimizing performance

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const compression = require("compression");
 const app = express();
 const port = 3000;
 
+app.use(compression());
+
+app.use("/products/:productid", express.static("dist"));
 app.get("/", (req, res) => {
   res.redirect("/products/8");
 });
-
-app.use("/products/:productid", express.static("dist"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
