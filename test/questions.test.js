74f3e80676bd0534924b98909e2850b3972cf535
Evelyn Chen <evelync93@gmail.com>
2019-10-31 16:23:58 -0400

    update tests

--- test/questions.test.js
+++ test/questions.test.js
@@ -8,13 +8,13 @@ import QuestionAnswer from "../src/components/questionComponents/QuestionAnswer.
 import QASet from "../src/components/questionComponents/QASet.jsx";
 import App from "../src/components/App.jsx";
 
-// let samplet2 = (a, b) => {
-//   return a + b;
-// };
+let samplet2 = (a, b) => {
+  return a + b;
+};
 
-// test("adds 1+2 to equal to 3", () => {
-//   expect(samplet2(1, 5)).toBe(6);
-// });
+test("adds 1+2 to equal to 3", () => {
+  expect(samplet2(1, 5)).toBe(6);
+});
 
 // describe("Questions", () => {
 //   it("should render max 2 questions on load", () => {
